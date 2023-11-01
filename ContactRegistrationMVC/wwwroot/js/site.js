eccb163e36d5875a91a5b5726e6bc66cde566710
Rafael Gomes <rafael.agomes@bancobmg.com.br>
2023-11-01 11:47:46 -0300

    creating pagination and filters

--- ContactRegistrationMVC/wwwroot/js/site.js
+++ ContactRegistrationMVC/wwwroot/js/site.js
@@ -3,6 +3,10 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function () {
+    $('#table-contacts').DataTable();
+})
+
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
